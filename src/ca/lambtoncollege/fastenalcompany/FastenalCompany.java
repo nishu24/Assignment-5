4fc3a31089c3e077dad3e07999b4e1c045b3ea79
shitong zhao <c0650224@lambtoncollege.ca>
2015-01-14 16:28:29 -0500

    add main method

--- src/ca/lambtoncollege/fastenalcompany/FastenalCompany.java
+++ src/ca/lambtoncollege/fastenalcompany/FastenalCompany.java
@@ -6,6 +6,9 @@
 package ca.lambtoncollege.fastenalcompany;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
 import java.util.Queue;
 
 /**
@@ -19,8 +22,18 @@ public class FastenalCompany {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-
+        Queue<Order> orders = new ArrayDeque<Order>();
+        orderArrives(orders);
+    }
+    
+    public static void orderArrives(Queue<Order> orders){
+        Order order = new Order();
+        Customer customer = new Customer(1,"jim");
+        order.setCustomer(customer);
+        List<Purchase>  purchases= new ArrayList<Purchase>();
+        purchases.add(new Purchase(1,1));
+        order.setPurchase(purchases);
+        orders.add(order);
     }
 
 }
