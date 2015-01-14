21a0e2524719787f4d53f60533d6c95eeda12e6b
c0646929 <c0646929@SB-N208-06.lambton.on.ca>
2015-01-14 15:42:14 -0500

    created customer class

---
+++ test/ca/lambtoncollege/FastenalCompany/CustomerTest.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ca.lambtoncollege.FastenalCompany;
+
+import ca.lambtoncollege.fastenalcompany.Customer;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Nisarg Panchal <neesarg@gmail.com>
+ */
+public class CustomerTest {
+    
+    public CustomerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getCustomerID method, of class Customer.
+     */
+    @Test
+    public void testGetCustomerID() {
+        System.out.println("getCustomerID");
+        Customer instance = new Customer();
+        int expResult = 0;
+        int result = instance.getCustomerID();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setCustomerID method, of class Customer.
+     */
+    @Test
+    public void testSetCustomerID() {
+        System.out.println("setCustomerID");
+        int customerID = 0;
+        Customer instance = new Customer();
+        instance.setCustomerID(customerID);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getCustomerName method, of class Customer.
+     */
+    @Test
+    public void testGetCustomerName() {
+        System.out.println("getCustomerName");
+        Customer instance = new Customer();
+        String expResult = "";
+        String result = instance.getCustomerName();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setCustomerName method, of class Customer.
+     */
+    @Test
+    public void testSetCustomerName() {
+        System.out.println("setCustomerName");
+        String customerName = "";
+        Customer instance = new Customer();
+        instance.setCustomerName(customerName);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of toString method, of class Customer.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+        Customer instance = new Customer();
+        String expResult = "";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
