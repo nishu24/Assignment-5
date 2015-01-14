49f265a512a507d87690fdea3f67eb29a58c5302
shitong zhao <c0650224@lambtoncollege.ca>
2015-01-14 15:21:36 -0500

    add class order

---
+++ src/fastenalcompany/Order.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fastenalcompany;
+
+/**
+ *
+ * @author shitong zhao <alpseinstein@gmail.com>
+ */
+public class Order {
+    private Customer customer;
+    private String timeRecevied;
+    private String timeProcessed;
+    private String timeFulfilled;
+    private Purchase purchase;
+    private String note;
+
+    public Order() {
+    }
+
+    public Order(Customer customer, String timeRecevied, String timeProcessed, String timeFulfilled, Purchase purchase, String note) {
+        this.customer = customer;
+        this.timeRecevied = timeRecevied;
+        this.timeProcessed = timeProcessed;
+        this.timeFulfilled = timeFulfilled;
+        this.purchase = purchase;
+        this.note = note;
+    }
+    
+    
+    
+}
